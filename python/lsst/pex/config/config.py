afd9d8c1f5b401804784a68f10c0299351f35fc9
Nate Lust <nlust@astro.princeton.edu>
2019-07-19 09:17:52 -0500

    Export config meta class

--- python/lsst/pex/config/config.py
+++ python/lsst/pex/config/config.py
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__all__ = ("Config", "Field", "FieldValidationError")
+__all__ = ("Config", "ConfigMeta", "Field", "FieldValidationError")
 
 import io
 import os
