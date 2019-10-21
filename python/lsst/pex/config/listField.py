f44bf220d2a6ec07da4cc4ac8ed0d11cb521291c
csimpson <csimpson@gemini.edu>
2019-10-21 10:52:47 -1000

    add "single" parameter to allow a single instance of a dtype, rather than a list of such items

--- python/lsst/pex/config/listField.py
+++ python/lsst/pex/config/listField.py
@@ -268,6 +268,9 @@ class ListField(Field):
     deprecated : None or `str`, optional
         A description of why this Field is deprecated, including removal date.
         If not None, the string is appended to the docstring for this Field.
+    single : `bool`, optional
+        If ``single`` is `True`, a single object of ``dtype`` rather than a
+        list is okay.
 
     See also
     --------
@@ -294,6 +297,7 @@ class ListField(Field):
         minLength=None,
         maxLength=None,
         deprecated=None,
+        single=False,
     ):
         if isinstance(dtype, list):
             dtype = tuple(dtype)
@@ -323,9 +327,13 @@ class ListField(Field):
             raise ValueError("'itemCheck' must be callable")
 
         source = getStackFrame()
+        if single:
+            dtype_setup = (List,) + dtype if isinstance(dtype, tuple) else (List, dtype)
+        else:
+            dtype_setup = List
         self._setup(
             doc=doc,
-            dtype=List,
+            dtype=dtype_setup,
             default=default,
             check=None,
             optional=optional,
@@ -361,6 +369,9 @@ class ListField(Field):
         to disable checking the list's maximum length).
         """
 
+        self.single = single
+        """Control whether a single object of dtype is okay."""
+
     def validate(self, instance):
         """Validate the field.
 
@@ -386,7 +397,7 @@ class ListField(Field):
         """
         Field.validate(self, instance)
         value = self.__get__(instance)
-        if value is not None:
+        if not self.single and value is not None:
             lenValue = len(value)
             if self.length is not None and not lenValue == self.length:
                 msg = "Required list length=%d, got length=%d" % (self.length, lenValue)
@@ -409,7 +420,14 @@ class ListField(Field):
             at = getCallStack()
 
         if value is not None:
-            value = List(instance, self, value, at, label)
+            if not self.single or isinstance(value, (list, tuple)):
+                value = List(instance, self, value, at, label)
+            else:
+                value = _autocast(value, self.dtype)
+                try:
+                    self._validateValue(value)
+                except BaseException as e:
+                    raise FieldValidationError(self, instance, str(e))
         else:
             history = instance._history.setdefault(self.name, [])
             history.append((value, at, label))
@@ -432,7 +450,10 @@ class ListField(Field):
             Plain `list` of items, or `None` if the field is not set.
         """
         value = self.__get__(instance)
-        return list(value) if value is not None else None
+        if isinstance(value, List):
+            return list(value)
+        else:
+            return value
 
     def _compare(self, instance1, instance2, shortcut, rtol, atol, output):
         """Compare two config instances for equality with respect to this
@@ -476,9 +497,14 @@ class ListField(Field):
             return False
         if l1 is None and l2 is None:
             return True
+        equal = True
+        if not isinstance(l1, List):
+            if not isinstance(l2, List):
+                return compareScalars(name, l1, l2, dtype=self.dtype[1], rtol=rtol, atol=atol, output=output)
+            else:
+                return False
         if not compareScalars("size for %s" % name, len(l1), len(l2), output=output):
             return False
-        equal = True
         for n, v1, v2 in zip(range(len(l1)), l1, l2):
             result = compareScalars(
                 "%s[%d]" % (name, n), v1, v2, dtype=self.dtype, rtol=rtol, atol=atol, output=output
